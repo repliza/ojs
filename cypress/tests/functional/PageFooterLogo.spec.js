9853998a12d2951bc00efe8f43cfcec998483371
j1shin <cradle-of-mail@gmx.de>
2020-05-30 00:22:03 +0200

    - added functional test for page footer logo customization - submodule plugins/generic/extendedSubmissionSettings, updated to commit 65cb00f7 of branch master - submodule plugins/themes/healthSciences, updated to commit 2457ce7c of branch master

---
+++ cypress/tests/functional/PageFooterLogo.spec.js
@@ -0,0 +1,26 @@
+describe('Page footer logo customization tests', function() {
+	it('Frontend', function() {
+		cy.visit('/');
+
+		cy.get('[data-cy=page-footer-logo-img]');
+	});
+
+	it('Dashboard', function() {
+		let user = Cypress.env('ADMIN_USER');
+		cy.login(user, Cypress.env('ADMIN_PASSWORD'));
+
+		// forcing click otherwise Cypress will complain about a display: none because it does not seem to understand media queries
+		cy.get('a').contains(user).click({force: true});
+		cy.get('a').contains('Dashboard').click({force: true});
+
+		cy.get('[data-cy=page-footer-logo-img]');
+	});
+
+	/*
+	it('Review, Get all submission details', function() {
+		cy.visit('/');
+
+		cy.get('[data-cy=page-footer-logo-img]');
+	});
+	*/
+
